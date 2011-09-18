79ffb1b18bdc138f47e7f7779bdd61891ad8bcda
Ray Donnelly <mingw.android@gmail.com>
2011-09-18 16:52:45 +0100

    added build scripts for kernel and rootfs (recreates+patches vgrades)
    config change in config-meego

---
+++ build-kernel.sh
@@ -0,0 +1,64 @@
+#!/bin/bash -e
+
+if [ "$1" = "" ] ; then
+    CONFIG=config-meego
+else
+    CONFIG=$1
+fi
+
+if [ "$2" = "" ] ; then
+    KERNEL_DEST=/media/sf_C_DRIVE/MeeGo
+else
+    KERNEL_DEST=$2
+fi
+
+if [ "$3" = "" ] ; then
+    ROOTFS_DEST=/media/meego-sd
+else
+    ROOTFS_DEST=$3
+fi
+
+if [ "$4" = "" ] ; then
+    CC=arm-linux-gnueabi-
+else
+    CC=$4
+fi
+
+ARCH=$(uname -m)
+CORES=1
+if test "-$ARCH-" = "-x86_64-" || test "-$ARCH-" = "-i686-"
+then
+    CORES=$(cat /proc/cpuinfo | grep processor | wc -l)
+    let CORES=$CORES+1
+fi
+
+echo ""; echo "Building kernel"; echo ""
+make clean
+rm -rf $ROOTFS_DEST/lib/modules/*
+rm arch/arm/boot/compressed/lib1funcs.S
+cp configs/$CONFIG .config
+rm -rf deploy
+mkdir deploy
+make -j${CORES} ARCH=arm LOCALVERSION= CROSS_COMPILE="${CCACHE} ${CC}" CONFIG_DEBUG_SECTION_MISMATCH=y zImage
+make -j${CORES} ARCH=arm LOCALVERSION= CROSS_COMPILE="${CCACHE} ${CC}" CONFIG_DEBUG_SECTION_MISMATCH=y modules
+cp arch/arm/boot/zImage deploy/zImage
+make ARCH=arm CROSS_COMPILE=${CC} modules_install INSTALL_MOD_PATH=deploy/
+sudo make ARCH=arm CROSS_COMPILE=${CC} modules_install INSTALL_MOD_PATH=$ROOTFS_DEST/
+
+# Although not used, copy it to the rootfs boot folder anyway.
+sudo cp deploy/zImage $ROOTFS_DEST/boot
+sudo cp deploy/zImage KERNEL_DEST
+
+echo ""; echo "Building atheros ar6000 module"; echo ""
+pushd atheros/ar6k_sdk
+./build.sh
+sudo cp host/os/linux/ar6000.ko $ROOTFS_DEST/
+sudo cp host/os/linux/ar6000.ko ../../deploy/
+popd
+
+sync
+sync
+
+echo ""; echo "All done!"; echo ""
+
+exit 0
---
+++ build-rootfs.sh
@@ -0,0 +1,142 @@
+#!/bin/bash -e
+
+# This script
+
+# reformat
+# recopy
+# remodify
+
+REMOUNT=0
+REFORMAT=0
+RECOPY=0
+REMODIFY=0
+
+function unMountIfMounted
+{
+echo $1
+    while sudo grep $1 /etc/mtab &>/dev/null
+    do
+        OLDMOUNT=`sudo grep -m1 $1 /etc/mtab | awk '{print $2}'`
+        echo $OLDMOUNT
+        echo umount $OLDMOUNT
+        sudo umount $OLDMOUNT
+    done
+}
+
+function exitShowingUsage
+{
+    echo "Hard coded script for patching vgrade's rootfs image"
+    echo "Not useful for anything else. Assumes you've got a"
+    echo "sd card and mg-tablet-tegra.sfdisk from vgrade's fs"
+    echo "and patches it with some parts (including tests"
+    echo "Very hacky, you may need to run it a few times before"
+    echo "it unmounts things properly!"
+    echo ""
+    echo "Usage build-rootfs.sh {reformat|recopy|remodify|remount} device"
+    echo " e.g. build-rootfs.sh reformat sdb"
+    echo "     : reformat - reformats sd card, recopies mg-tablet-tegra.img, patches with meego-root-fs-vgrade-mods (very slow)"
+    echo "     : recopy   - recopies mg-tablet-tegra.img, patches with meego-root-fs-vgrade-mods (very slow)"
+    echo "     : remodify - patches with meego-root-fs-vgrade-mods (fast)"
+    echo "     : remount  - just mounts /media/meego-sd (fast)"
+    exit 1
+}
+
+if [ "$1" = "reformat" ] ;
+then
+    REMOUNT=1
+    REFORMAT=1
+    RECOPY=1
+    REMODIFY=1
+else
+    if [ "$1" = "recopy" ] ;
+    then
+        REMOUNT=1
+        REFORMAT=0
+        RECOPY=1
+        REMODIFY=1
+    else
+        if [ "$1" = "remodify" ] ;
+        then
+            REMOUNT=1
+            REFORMAT=0
+            RECOPY=0
+            REMODIFY=1
+        else
+            if [ "$1" = "remount" ] ;
+            then
+                REMOUNT=1
+                REFORMAT=0
+                RECOPY=0
+                REMODIFY=0
+            else
+                exitShowingUsage
+            fi
+        fi
+    fi
+fi
+
+if [ "$REFORMAT" = "0" -a "$RECOPY" = "0" -a "$REMODIFY" = "0" -a "$REMOUNT" = "0" ] ; then
+    exitShowingUsage
+fi
+
+if [ "$2" == "" ] ; then
+echo "Error: Device not specified!"
+    exitShowingUsage
+fi
+
+DEVICE=$2
+
+if [ -z /dev/$DEVICE ] ; then
+    echo "Error: /dev/$DEVICE not found!"
+    exitShowingUsage
+fi
+
+if [ "$REMOUNT" = "1" ] ; then
+    unMountIfMounted /dev/${DEVICE}1
+    unMountIfMounted /dev/${DEVICE}2
+    unMountIfMounted /dev/mapper/loop0p2
+
+    if [ "$REFORMAT" = "1" ] ; then
+        if [ ! -f ../mg-tablet-tegra.sfdisk ] ; then
+            echo "Error: Couldn't open ../mg-tablet-tegra.sfdisk!"
+            exitShowingUsage
+        fi
+        sudo sfdisk -d /dev/${DEVICE} < mg-tablet-tegra.sfdisk
+        sudo mkfs -t ext3 /dev/${DEVICE}2
+        sudo tune2fs -i 0 /dev/${DEVICE}2
+        sudo tune2fs -c 0 /dev/${DEVICE}2
+    fi
+
+    if [ ! -d /media/meego-sd ] ; then
+       sudo mkdir /media/meego-sd
+    fi
+
+    sudo mount -t ext3 /dev/${DEVICE}2 /media/meego-sd -O "rw,nosuid,nodev,uhelper=udisks"
+
+    if [ "$?" != "0" ] ; then
+        echo "Error: Couldn't mount /dev/${DEVICE}2 at /media/meego-sd!"
+        exitShowingUsage
+    fi
+fi
+
+if [ "$RECOPY" = "1" ] ; then
+    exit 1
+    if [ ! -f ../mg-tablet-tegra.img ] ; then
+        echo "Error: Couldn't open ../mg-tablet-tegra.img"
+        exitShowingUsage
+    fi
+    if [ "$REFORMAT" = "0" ] ; then
+        sudo rm -rf /media/meego-sd/*
+    fi
+    sudo kpartx -v -a ../mg-tablet-tegra.img
+    sudo mount /dev/mapper/loop0p2 /media/meego/
+    sudo rsync -axS --exclude=/tmp/* --progress /media/meego/* /media/meego-sd
+fi
+
+if [ "$REMODIFY" = "1" ] ; then
+    sudo rsync -axS --progress meego-root-fs-vgrade-mods/* /media/meego-sd
+fi
+
+echo ""; echo "All done!"; echo ""
+
+exit 0
